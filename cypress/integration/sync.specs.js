65f33987f2777166936649f720d5b1ab376337e0
setonelf <freitas.war@gmail.com>
2024-02-26 15:11:00 -0300

    Adcionando testes com timeout e wait

--- cypress/integration/sync.specs.js
+++ cypress/integration/sync.specs.js
@@ -26,13 +26,30 @@ describe('Esperas...', ()=>{
         .type('funciona')
    })
 
-   it.only('Uso do find', ()=>{
+   it('Uso do find', ()=>{
     cy.get('#buttonList').click()
     cy.get('#lista li')
         .find('span')
         .should('contain', 'Item 1')
     cy.get('#lista li span')
         .should('contain', 'Item 2')
+   })
+
+   it('Teste com uso do timeout e wait', ()=>{
+        cy.get('#buttonDelay').click()
+        //cy.get('#novoCampo').should('exist')
+        cy.get('#novoCampo', {timeout: 5000}).should('exist')
+        //caso deseja mudar o timeout para toda aplicação, ir no arquivo cypress.json e
+        //adicionar o argumento "defaultCommandTimeout: 1000", onde o 1000 é o tempo em milisegundos
+        cy.get('#buttonListDOM').click()
+        //cy.wait(5000)
+        cy.get('#lista li span')
+            .should('have.length', 1)
+        cy.get('#lista li span')
+            .should('have.length', 2)
+            //.should('contain', 'Item 2')
+
+        
    })
 
    
