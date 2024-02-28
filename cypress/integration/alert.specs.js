50d774d1c993776d8edccfac206959449f658598
setonelf <freitas.war@gmail.com>
2024-02-28 13:30:24 -0400

    Adicionando tratativa para confirm alert para pressionar oK ou Cancel

--- cypress/integration/alert.specs.js
+++ cypress/integration/alert.specs.js
@@ -18,11 +18,39 @@ describe('Work with alerts', ()=>{
         })
     })
 
-    it.only('Alert com mock', ()=>{
+    it('Alert com mock', ()=>{
         const stub = cy.stub().as('alerta')
         cy.on('window:alert', stub)
         cy.get('#alert').click().then(()=> {
             expect(stub.getCall(0)).to.be.calledWith('Alert Simples')
         })
+    })
+
+    it('Alert Confirm', ()=>{
+        
+        cy.on('window:confirm', msg =>{
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+        })
+        cy.on('window:alert', msg =>{
+            console.log(msg)
+            expect(msg).to.be.equal('Confirmado')
+        })
+        cy.get('#confirm').click()
+    })
+
+    it.only('Alert Deny', ()=>{
+        
+        cy.on('window:confirm', msg =>{
+            console.log(msg)
+            expect(msg).to.be.equal('Confirm Simples')
+            // para apertar no botao de cancelar do confirm e necessario fazer um return false
+            return false
+        })
+        cy.on('window:alert', msg =>{
+            console.log(msg)
+            expect(msg).to.be.equal('Negado')
+        })
+        cy.get('#confirm').click()
     })
 })
