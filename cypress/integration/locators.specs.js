95b3cc6bb0911f2e82dea28d8109a406f8f520f5
setonelf <freitas.war@gmail.com>
2024-03-04 12:51:54 -0300

    Adicionando arquivo com uso de xpath para o cypress

--- cypress/integration/locators.specs.js
+++ cypress/integration/locators.specs.js
@@ -12,4 +12,11 @@ describe('elements', ()=>{
     it('Usando Jquery Selector',()=>{
         cy.get('[onclick*="Francisco"]').click()
     })
+
+    it.only('using Xpath', ()=>{
+        cy.xpath('//input[contains(@onclick, \'Francisco\')]')
+        cy.xpath("//table[@id='tabelaUsuarios']//td[contains(., 'Francisco')]/following-sibling::td/input[@type='text']")
+        cy.xpath("//table[@id='tabelaUsuarios']//td[contains(., 'Francisco')]/..//input[@type='text']")
+        cy.xpath("//td[contains(., 'Usuario A')]/following-sibling::td[contains(., 'Mestrado')]/..//input[@type='text']").type("Funciona")
+    })
 }) 
